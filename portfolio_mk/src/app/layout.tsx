d519ef4d195074a8cfc5d2076c4589d864eff4c1
mansi104-ai <mkb.kalra@gmail.com>
2024-06-01 13:27:49 +0530

    to be deployed on vercel

---
+++ portfolio_mk/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Mansi's portfolio",
+  description: "Journey ahead, don't look back",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
